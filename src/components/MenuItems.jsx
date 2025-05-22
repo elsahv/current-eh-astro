f421c9faf3ce4e01c3098fef0e17bd51a283231e
elsahv <elsahvy@gmail.com>
2025-05-21 17:53:59 -0700

    rm unneeded content

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,5 +1,5 @@
 const testitems = [
-  { item1: "about", link: "/#about" },
+  // { item1: "about", link: "/#about" },
   { item1: "featured work", link: "/#featured-work" },
   { item1: "creative concepts", link: "/#creative-concepts " },
   { item1: "contact", link: "/#contact" },
